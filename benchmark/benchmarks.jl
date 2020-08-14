7260f7078250f655b322a79d857ec600c1aa54e3
Luke Adams <lukeclydeadams@gmail.com>
2020-08-14 12:21:28 -0600

    update benchmarks to new name

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,4 +1,4 @@
-using ElectrostaticTest
+using ParticleInCell
 using BenchmarkTools
 
 const SUITE = BenchmarkGroup()
@@ -6,8 +6,8 @@ const SUITE = BenchmarkGroup()
 num_cells = 32
 num_guard_cells = 2
 simulation_length = 1.
-g1 = ElectrostaticTest.UniformGrid(32, 2, 1.)
-f1 = fill(0., ElectrostaticTest.total_cells(g1))
+g1 = ParticleInCell.UniformGrid(32, 2, 1.)
+f1 = fill(0., ParticleInCell.total_cells(g1))
 
 num_macros = 640
 p1  = collect(range(0, stop=simulation_length, length=num_macros+1))[1:num_macros]
@@ -15,21 +15,21 @@ p1  = collect(range(0, stop=simulation_length, length=num_macros+1))[1:num_macro
 num_cells = 128
 num_guard_cells = 2
 simulation_length = 1.
-g2 = ElectrostaticTest.UniformGrid(32, 2, 1.)
-f2 = fill(0., ElectrostaticTest.total_cells(g1))
+g2 = ParticleInCell.UniformGrid(32, 2, 1.)
+f2 = fill(0., ParticleInCell.total_cells(g1))
 
 num_macros = 10000
 p2  = collect(range(0, stop=simulation_length, length=num_macros+1))[1:num_macros]
 
-shape_functions = [ElectrostaticTest.shape_1st_order,
-                   ElectrostaticTest.shape_2nd_order]
+shape_functions = [ParticleInCell.shape_1st_order,
+                   ParticleInCell.shape_2nd_order]
 
 SUITE["scatter"] = BenchmarkGroup()
 for shape_function in shape_functions
     SUITE["scatter"][string(shape_function), 1] =
-        @benchmarkable ElectrostaticTest.scatter_charge_to_grid!($p1, $f1, $g1, 1., $shape_function)
+        @benchmarkable ParticleInCell.scatter_charge_to_grid!($p1, $f1, $g1, 1., $shape_function)
     SUITE["scatter"][string(shape_function), 2] =
-        @benchmarkable ElectrostaticTest.scatter_charge_to_grid!($p2, $f2, $g2, 1., $shape_function)
+        @benchmarkable ParticleInCell.scatter_charge_to_grid!($p2, $f2, $g2, 1., $shape_function)
 end
 
 
@@ -58,4 +58,4 @@ f2 = similar(f1)
 
 SUITE["field_solve"] = BenchmarkGroup()
 SUITE["field_solve"]["fourier"] =
-    @benchmarkable ElectrostaticTest.fourier_field_solve!(f1, f2, ft_vector, ksq_inv, offset, num_cells)
+    @benchmarkable ParticleInCell.fourier_field_solve!(f1, f2, ft_vector, ksq_inv, offset, num_cells)
