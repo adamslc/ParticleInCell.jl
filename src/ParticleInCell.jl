8c18005dd697937e849f4d4439946920a0e3a2c8
Luke Adams <lukeclydeadamsdev@gmail.com>
2020-09-17 14:30:43 -0600

    Record timing information

--- src/ParticleInCell.jl
+++ src/ParticleInCell.jl
@@ -26,8 +26,14 @@ mutable struct Simulation{FT, IT}
     fields::Vector{Field}
 
     timestep::FT
+    num_timesteps::IT
     time::FT
     integration_steps::Vector{IntegrationStep}
+
+    total_time::FT
+    integration_times::Vector{FT}
+
+    setup_complete::Bool
 
     function Simulation(timestep::FT) where FT
         num_species = 0
@@ -42,8 +48,10 @@ mutable struct Simulation{FT, IT}
         time = zero(FT)
         integration_steps = Vector{IntegrationStep}()
 
+        integration_times = Vector{FT}()
+
         return new{FT, Int64}(num_species, num_fields, species_dict, fields_dict, species,
-                   fields, timestep, time, integration_steps)
+                              fields, timestep, 0, time, integration_steps, zero(FT), integration_times, false)
     end
 end
 
@@ -61,24 +69,54 @@ function add_field!(sim::Simulation{FT, IT}, field::Field{FT},
     sim.fields_dict[label] = sim.num_fields
 end
 
-function add_integration_step!(sim::Simulation, step::IntegrationStep)
+function add_integration_step!(sim::Simulation{FT}, step::IntegrationStep) where FT
     push!(sim.integration_steps, step)
+    push!(sim.integration_times, zero(FT))
 end
 
 function setup!(sim::Simulation)
     for step in sim.integration_steps
         setup!(step, sim)
     end
+    sim.setup_complete = true
 end
 setup!(::IntegrationStep, ::Simulation) = nothing
 
 function step!(sim::Simulation)
-    for step in sim.integration_steps
+    start_time = time()
+    for (i, step) in enumerate(sim.integration_steps)
+        step_start_time = time()
         step!(step, sim)
+        sim.integration_times[i] += time() - step_start_time
     end
     sim.time += sim.timestep
+    sim.num_timesteps += 1
+    sim.total_time += time() - start_time
 end
 step!(::IntegrationStep, ::Simulation) = throw("No step method")
+
+function print_summary(io::IO, sim::Simulation{FT, IT}) where {FT, IT}
+    println(io, "Simulation{$FT, $IT}")
+    println(io, "Simulation setup ", sim.setup_complete ? "has" : "has not", " been completed.")
+    println(io, "Simulated $(sim.time) seconds using $(sim.num_steps) timesteps".)
+#    println(io, "Total simulation wall time is $(sim.total_time) seconds.")
+#    println(io)
+#    println(io, "  $num_species species")
+#    for s in keys(sim.species_dict)
+#        println(io, "    ", s)
+#    end
+#    println(io)
+#    println(io, "  $num_fields fields")
+#    for f in keys(sim.fields_dict)
+#        println(io, "    ", f)
+#    end
+#    println(io)
+#    println(io, "  $(length(integration_steps)) integration steps")
+#    for (i, s) in enumerate(sim.integration_steps)
+#        println(io, "    $s")
+#        println(io, "      $(sim.integration_times[i]) seconds ($(sim.integration_times[i] / sim.total_time * 100)%)")
+#    end
+end
 
 include("shape_functions.jl")
 include("scatter.jl")
