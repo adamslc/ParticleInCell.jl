4114f1c7aec166e3a0136115edbb0a32db804bbd
Luke Adams <luke@lukeclydeadams.com>
2020-09-18 13:09:01 -0600

    Update Langmuir oscillation example

--- examples/langmuir_oscillation/plot.jl
+++ examples/langmuir_oscillation/plot.jl
@@ -3,42 +3,9 @@ using HDF5
 using Makie
 using MakieLayout
 
-function update_plot(scene, layout, i)
-    @info "Reading data for dump $i"
-    f = h5open("data/dump_$i.h5", "r")
-    positions = f["positions"][:]
-    velocities = f["velocities"][:]
-
-    grid_pos = f["grid_pos"][:]
-    rho = f["rho"][:]
-    phi = f["phi"][:]
-    elec = f["elec"][:]
-
-    grid_pos_hd = f["grid_pos_hd"][:]
-    rho_hd = f["rho_hd"][:]
-
-    @info "Updating plots"
-    ax1 = layout[1, 1] = LAxis(scene)
-    xlims!(ax1, 0, 1)
-    ylims!(ax1, -1.2e5, 1.2e5)
-    electrons = scatter!(ax1, positions, velocities, color=:blue, markersize=2px)
-
-    layout[1, 2] = LLegend(scene, [electrons], ["Electrons"])
-
-    ax2 = layout[2, 1] = LAxis(scene)
-    xlims!(ax2, 0, 1)
-    ylims!(ax1, -0.001, 0.001)
-    rp = plot!(ax2, grid_pos, rho, color=:red)
-    rp_hd = plot!(ax2, grid_pos_hd, rho_hd, color=:orange)
-
-    layout[2, 2] = LLegend(scene, [rp, rp_hd], ["Rho", "HD Rho"])
-
-    return
-end
-
-function create_pts(i, xlabel, ylabel)
+function create_pts(i, xlabel, ylabel, prefix)
     @debug "Reading data for dump $i"
-    f = h5open("data/dump_$i.h5", "r")
+    f = h5open("$(prefix)_$i.h5", "r")
     xvals = f[xlabel][:]
     yvals = f[ylabel][:]
     close(f)
@@ -46,41 +13,109 @@ function create_pts(i, xlabel, ylabel)
     return [Point(x, y) for (x, y) in zip(xvals, yvals)]
 end
 
-@info "Creating scene"
-scene, layout = layoutscene(10, resolution=(1200, 700))
-display(scene)
+function create_display(prefix)
+    directory = prefix[1:findlast('/', prefix)-1]
+    files = readdir(directory)
+    num_dumps = 0
+    for file in files
+        if startswith(file, prefix[findlast('/', prefix)+1:end] * "_")
+            num_dumps += 1
+        end
+    end
 
-@info "Populating scene"
-ax1 = layout[1,1] = LAxis(scene)
-ax2 = layout[2,1] = LAxis(scene)
-linkxaxes!(ax1, ax2)
-xlims!(ax1, 0, 1)
+    @info "Creating scene" num_dumps
+    scene, layout = layoutscene(10)
+    display(scene)
 
-slide = LSlider(scene, range=0:100, startvalue=0)
+    @info "Populating scene"
+    ax1 = layout[1,1] = LAxis(scene, xgridvisible=false, ygridvisible=false)
+    ax2 = layout[2,1] = LAxis(scene, xgridvisible=false, ygridvisible=false)
+    ax3 = layout[3,1] = LAxis(scene, xgridvisible=false, ygridvisible=false)
+    linkxaxes!(ax1, ax2, ax3)
+    xlims!(ax1, 0, 1)
 
-slider_text = lift(i -> "Dump=$i", slide.value)
-text_obj = LText(scene, slider_text)
+    slide = LSlider(scene, range=0:num_dumps-1, startvalue=0)
+    slider_text = lift(i -> "Dump=$i", slide.value)
+    text_obj = LText(scene, slider_text)
+    controls = layout[end+1, :] = GridLayout(tellwidth=false)
+    controls[:h] = [slide, text_obj]
 
-controls = layout[end+1, :] = GridLayout(tellwidth=false)
-controls[:h] = [slide, text_obj]
+    dist_pts = lift(i -> create_pts(i, "positions", "velocities", prefix), slide.value)
+    electrons = scatter!(ax1, dist_pts, color=:blue, markersize=2px)
 
-dist_pts = lift(i -> create_pts(i, "positions", "velocities"), slide.value)
-electrons = scatter!(ax1, dist_pts, color=:blue, markersize=2px)
+    f = h5open("$(prefix)_0.h5", "r")
+    pos0 = f["positions"][:]
+    vel0 = f["velocities"][:]
+    close(f)
+    dist0 = lines!(ax1, pos0, vel0, color=:black, linewidth=0.5, linestyle=:dash)
 
-layout[1, 2] = LLegend(scene, [electrons], ["Electrons"])
+    layout[1, 2] = LLegend(scene, [electrons, dist0], ["Electrons", "Initial distribution"])
 
-rho_pts = lift(i -> create_pts(i, "grid_pos", "rho"), slide.value)
-rp = lines!(ax2, rho_pts, color=:red)
+    rho_pts = lift(i -> create_pts(i, "grid_pos", "rho", prefix), slide.value)
+    rp = lines!(ax2, rho_pts, color=:red, xautolimits=false)
 
-rho_hd_pts = lift(i -> create_pts(i, "grid_pos_hd", "rho_hd"), slide.value)
-rp_hd = lines!(ax2, rho_hd_pts, color=:orange)
+    rho_hd_pts = lift(i -> create_pts(i, "grid_pos_hd", "rho_hd", prefix), slide.value)
+    rp_hd = lines!(ax2, rho_hd_pts, color=:orange, xautolimits=false)
 
-layout[2, 2] = LLegend(scene, [rp, rp_hd], ["Rho", "HD Rho"])
+    layout[2, 2] = LLegend(scene, [rp, rp_hd], ["Rho", "HD Rho"])
 
-on(scene.events.keyboardbuttons) do but
-    ispressed(but, Keyboard.left)  && set_close_to!(slide, slide.value[]-1)
-    ispressed(but, Keyboard.right) && set_close_to!(slide, slide.value[]+1)
+    elec_pts = lift(i -> create_pts(i, "grid_pos", "elec", prefix), slide.value)
+    ep = lines!(ax3, elec_pts, color=:red, xautolimits=false)
+
+    layout[3, 2] = LLegend(scene, [ep], ["E"])
+
+    f = h5open("$(prefix)_$(slide.value[]).h5", "r")
+    grid_pos = f["grid_pos"][:]
+    close(f)
+    lims1 = lift(rect -> [rect.origin[2], rect.origin[2] + rect.widths[2]], ax1.limits)
+    lims2 = lift(rect -> [rect.origin[2], rect.origin[2] + rect.widths[2]], ax2.limits)
+    lims3 = lift(rect -> [rect.origin[2], rect.origin[2] + rect.widths[2]], ax3.limits)
+    for g in grid_pos
+        lines!(ax1, [g, g], lims1, color=:black, linewidth=0.5, linestyle=:dot, xautolimits=false, yautolimits=false)
+        lines!(ax2, [g, g], lims2, color=:black, linewidth=0.5, linestyle=:dot, xautolimits=false, yautolimits=false)
+        lines!(ax3, [g, g], lims3, color=:black, linewidth=0.5, linestyle=:dot, xautolimits=false, yautolimits=false)
+    end
+
+    on(scene.events.keyboardbuttons) do but
+        ispressed(but, Keyboard.left)  && set_close_to!(slide, slide.value[]-1)
+        ispressed(but, Keyboard.right) && set_close_to!(slide, slide.value[]+1)
+    end
+
+    return scene, layout
 end
 
-@info "Ready. Press [ENTER] to exit."
-readline()
+function ui(default_str)
+    old_str = default_str
+    scene = nothing
+    layout = nothing
+    while true
+        print("> ")
+        str = readline()
+
+        if str == "exit" || str == "quit"
+            println("Goodbye")
+            exit()
+        elseif str == "save"
+            print("Enter filename: ")
+            plot_str = readline()
+            Makie.save(plot_str, scene)
+        elseif str == ""
+            try
+                scene, layout = create_display(old_str)
+            catch
+                println("ERROR")
+            end
+        else
+            old_str = str
+            try
+                scene, layout = create_display(str)
+            catch
+                println("ERROR")
+            end
+        end
+    end
+
+    return
+end
+
+ui("data/dump")
