21c7e18809201518b1fbeb4e5e84842b4e266370
Luke Adams <luke@lukeclydeadams.com>
2020-09-29 23:26:56 -0600

    Update gather step to support arbitrary charges
    In particular, this allows us to reuse this step for energy conserving
    algorithms.

--- src/gather.jl
+++ src/gather.jl
@@ -1,4 +1,4 @@
-mutable struct GatherForcesFromGrid{IT, S} <: IntegrationStep
+mutable struct GatherForcesFromGrid{FT, IT, S} <: IntegrationStep
     species_name::String
     field_name::String
 
@@ -6,20 +6,25 @@ mutable struct GatherForcesFromGrid{IT, S} <: IntegrationStep
     field_index::IT
 
     shape_function::S
+    charge::FT
 end
 
 function GatherForcesFromGrid(species_name::String, field_name::String,
-                             shape_function::S=shape_1st_order) where {S}
+                              shape_function::S=shape_1st_order, charge::FT=0.) where {FT, S}
     species_index = -1
     field_index = -1
 
-    return GatherForcesFromGrid{Int, S}(species_name, field_name, species_index,
-                                field_index, shape_function)
+    return GatherForcesFromGrid{FT, Int, S}(species_name, field_name, species_index,
+                                field_index, shape_function, charge)
 end
 
-function setup!(step::GatherForcesFromGrid, sim::Simulation)
+function setup!(step::GatherForcesFromGrid{FT}, sim::Simulation{FT}) where {FT}
     step.species_index = sim.species_dict[step.species_name]
     step.field_index = sim.fields_dict[step.field_name]
+
+    if step.charge == zero(FT)
+        step.charge = sim.species[step.species_index].macro_charge
+    end
 end
 
 
@@ -28,7 +33,7 @@ function step!(step::GatherForcesFromGrid, sim::Simulation)
     forces = sim.species[step.species_index].forces
     field = sim.fields[step.field_index].grid_values
     grid = sim.fields[step.field_index].grid
-    charge = sim.species[step.species_index].macro_charge
+    charge = step.charge
 
     for (i, x) in enumerate(positions), (grid_index, grid_pos) in grid
         forces[i] += charge * field[grid_index] * step.shape_function((x - grid_pos) / cell_length(grid, grid_index))
