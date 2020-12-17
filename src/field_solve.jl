9ef620ed08732cf1638833478daa3aa82839d195
Luke Adams <luke@lukeclydeadams.com>
2020-12-17 12:22:23 -0700

    Cleanup deadcode in field_solve.jl

--- src/field_solve.jl
+++ src/field_solve.jl
@@ -49,7 +49,7 @@ function step!(step::FourierFieldSolve, sim::Simulation)
     num_guard_cells = sim.fields[step.input_field_index].grid.num_guard_cells
 
     offset = num_guard_cells
-    # Setup ft_vector to Fourier tramsform
+    # Setup ft_vector to Fourier transform
     for i in 1:num_cells
         step.ft_vector[i] = input_values[i + offset]
     end
@@ -89,47 +89,6 @@ function step!(step::FourierFieldSolve, sim::Simulation)
     for j in 1:grid.num_guard_cells
         output_values[grid.num_guard_cells + grid.num_cells + 1 + j] = output_values[grid.num_guard_cells + 1 + j]
     end
-end
-
-function fourier_field_solve!(f1, f2, ft_vector, ksq_inv, offset, num_cells)
-    # Setup ft_vector to Fourier tramsform
-    for i in 1:num_cells
-        ft_vector[i] = f1[i + offset]
-    end
-
-    # Compute FFT in place
-    fft!(ft_vector)
-
-    # We assume that the plasma is charge neutral, so this mode must be zero.
-    ft_vector[1] = 0
-
-    # The modes 2:num_cells (of which there are num_cells - 1) are paired in conjugate
-    # pairs so rhok[2] = complex_conjugate(rhok[end]) and so on. Thus we need
-    # to multiply both by the same scaling factor.
-    for i in 1:round(Int, num_cells / 2)
-        k1 = i + 1
-        k2 = num_cells + 1 - i
-        ft_vector[k1] = ksq_inv[i] * ft_vector[k1]
-        k1 == k2 && break
-        ft_vector[k2] = ksq_inv[i] * ft_vector[k2]
-    end
-
-    # Compute IFFT in place
-    ifft!(ft_vector)
-
-    # Write values to output_field
-    for i in 1:num_cells
-        f2[i + offset] = real(ft_vector[i])
-    end
-
-    ## Update guard/periodic cells
-    #for j in 1:grid.num_guard_cells
-    #    f2[j] = f2[grid.num_cells + j]
-    #end
-    #f2[grid.num_guard_cells + grid.num_cells + 1] = f2[grid.num_guard_cells + 1]
-    #for j in 1:grid.num_guard_cells
-    #    f2[grid.num_guard_cells + grid.num_cells + 1 + j] = f2[grid.num_guard_cells + 1 + j]
-    #end
 end
 
 mutable struct FiniteDifferenceToNodes{IT} <: IntegrationStep
