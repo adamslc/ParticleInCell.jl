4c789f40d924162898fe5f7285e994cb7532dddb
Luke Adams <luke@lukeclydeadams.com>
2020-12-17 12:20:39 -0700

    Support custom charges in ScatterChargeToGrid

--- src/scatter.jl
+++ src/scatter.jl
@@ -1,4 +1,4 @@
-mutable struct ScatterChargeToGrid{IT, S} <: IntegrationStep
+mutable struct ScatterChargeToGrid{FT, IT, S} <: IntegrationStep
     species_name::String
     field_name::String
 
@@ -6,27 +6,32 @@ mutable struct ScatterChargeToGrid{IT, S} <: IntegrationStep
     field_index::IT
 
     shape_function::S
+    charge::FT
 end
 
 function ScatterChargeToGrid(species_name::String, field_name::String,
-                             shape_function::S=shape_1st_order) where {S}
+                             shape_function::S=shape_1st_order, charge::FT=0.) where {FT, S}
     species_index = -1
     field_index = -1
 
-    return ScatterChargeToGrid{Int, S}(species_name, field_name, species_index,
-                                field_index, shape_function)
+    return ScatterChargeToGrid{FT, Int, S}(species_name, field_name, species_index,
+                                field_index, shape_function, charge)
 end
 
-function setup!(step::ScatterChargeToGrid, sim::Simulation)
+function setup!(step::ScatterChargeToGrid{FT}, sim::Simulation) where {FT}
     step.species_index = sim.species_dict[step.species_name]
     step.field_index = sim.fields_dict[step.field_name]
+
+    if step.charge == zero(FT)
+        step.charge = sim.species[step.species_index].macro_charge
+    end
 end
 
 function step!(step::ScatterChargeToGrid, sim::Simulation)
     positions = sim.species[step.species_index].positions
     field = sim.fields[step.field_index].grid_values
     grid = sim.fields[step.field_index].grid
-    charge = sim.species[step.species_index].macro_charge
+    charge = step.charge
 
     scatter_charge_to_grid!(positions, field, grid, charge, step.shape_function)
 end
